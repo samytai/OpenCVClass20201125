489aee2b075055d41b925f8a155c8d522f3e3851
Sam Tai <ytai1123@gmail.com>
2020-11-25 14:54:42 +0800

    pyCharm OpenCV for Chinese support

--- demo10.py
+++ demo10.py
@@ -1,6 +1,8 @@
 import cv2
 import matplotlib.pyplot as plt
 import math
+from PIL import ImageFont, ImageDraw, Image
+import numpy as np
 
 IMAGE1 = 'images/bg1.jpg'
 orginalImage = cv2.imread(IMAGE1, -1)
@@ -26,11 +28,16 @@ def drawCircle(action,x,y,flags,userdata):
 myWindow = "main Window"
 cv2.namedWindow(myWindow)
 cv2.setMouseCallback(myWindow, drawCircle)
-
+FONT_PATH='fonts/NotoSerifCJKtc-Regular.otf'
+font=ImageFont.truetype(FONT_PATH, 36)
+img_pil=Image.fromarray(orginalImage)
+draw=ImageDraw.Draw(img_pil)
 
 k = 0  ##keyboard 事件
 while k != 27:
     cv2.imshow(myWindow, orginalImage)
+    draw.text((10,60),"我是中文.ㄅㄆㄇㄈ①②③④⑤⑥.",font=font, fill=(250,255,255,255))
+    orginalImage=np.array(img_pil)
     cv2.putText(orginalImage, "left click, and drag", (50, 50),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
     k = cv2.waitKey(20)
